a57313d04235a15298f6fa6ceafd6166f09c8992
Noland Seigler <nolandseigler@gmail.com>
2020-03-26 17:29:09 -0500

    db restructure

--- src/main/java/com/codeon/models/User.java
+++ src/main/java/com/codeon/models/User.java
@@ -11,9 +11,9 @@ public class User {
     private long id;
     @Column(nullable = false, length = 50, unique = true)
     private String username;
-    @Column(name = "first_name",nullable = false, length = 50, unique = true)
+    @Column(name = "first_name",nullable = false, length = 50)
     private String firstName;
-    @Column(name = "last_name", nullable = false, length = 50, unique = true)
+    @Column(name = "last_name", nullable = false, length = 50)
     private String lastName;
     @Column(nullable = false, length = 100, unique = true)
     private String email;

c4d66d4097787c5abc7d19e675f4344fbb643cfa
Noland Seigler <nolandseigler@gmail.com>
2020-03-27 13:34:47 -0500

    checkpoint

--- src/main/java/com/codeon/controllers/IndexController.java
+++ src/main/java/com/codeon/controllers/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-
+//What is this? -Noland
     @Controller
     public class IndexController {
 

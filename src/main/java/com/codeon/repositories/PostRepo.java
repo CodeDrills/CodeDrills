b742fcc4c465d522fed57d3f4a580aeea5829c61
William Stephens <william.a.stephens17@gmail.com>
2020-03-29 17:53:47 -0500

    Interview questions now sort to page interview-questions/interview-question. Singular because it will only be one question at a time when I write the logic

--- src/main/java/com/codeon/repositories/PostRepo.java
+++ src/main/java/com/codeon/repositories/PostRepo.java
@@ -10,11 +10,6 @@ public interface PostRepo extends JpaRepository<Post, Long> {
     public Post findPostById(long id);
     public Post findPostByTitle(String title);
     public Post findPostByPostTypeId(Long id);
-    public List<Post> findAllByPostTypeId(Long post_type_id);
-    public List<Post> findAllByPostTypeId_Type(String type);
 
-    //Selects only posts of type 'interview_question'
-    @Query(value = "select * from posts p join post_types t on p.post_type_id = t.id where t.type = 'interview_question'",
-           nativeQuery = true)
-    public List<Post> findAllPostsInterviewQuestions();
+    public List<Post> findAllByPostTypeId_Type(String type);
 }

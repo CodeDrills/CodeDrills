945e2683776c30d4a23ecbc8f8f5941829af00ec
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-03-30 09:58:56 -0500

    change

--- src/main/java/com/codeon/repositories/PostRepo.java
+++ src/main/java/com/codeon/repositories/PostRepo.java
@@ -2,7 +2,6 @@ package com.codeon.repositories;
 
 import com.codeon.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,6 +9,5 @@ public interface PostRepo extends JpaRepository<Post, Long> {
     public Post findPostById(long id);
     public Post findPostByTitle(String title);
     public Post findPostByPostTypeId(Long id);
-
-    public List<Post> findAllByPostTypeId_Type(String type);
+    List<Post> getAllPostbyusersid (long id);
 }

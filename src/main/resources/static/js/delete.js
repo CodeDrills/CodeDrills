453a8906202d2dc2d108f6f93a52e6989b01d878
Noland Seigler <nolandseigler@gmail.com>
2020-03-31 21:05:47 -0500

    still testing and troubleshooting js issues with loading the resources(which work from root page) and making delete requests. receiving 403. makes me thing it is the anti csrf but still doesnt work with csrf disabled...

--- src/main/resources/static/js/delete.js
+++ src/main/resources/static/js/delete.js
@@ -1,4 +1,8 @@
 
+// const token = $("meta[name='_csrf']").attr("content");
+// const header = $("meta[name='_csrf_header']").attr("content");
+const token = document.querySelector('meta[name="_csrf"]').content;
+// const header = document.querySelector('meta[name="_csrf_header"]').content;
 let deleteButtonClass = document.querySelectorAll(".delete-button");
 deleteButtonClass.forEach(button => {
     button.addEventListener("click", function() {
@@ -8,6 +12,9 @@ deleteButtonClass.forEach(button => {
         let postId = idSplit[2];
         fetch(`http://localhost:8080/${postType}/delete?id=${postId}`, {
             method: 'DELETE',
+            headers: {
+                'X-CSRF-TOKEN': token
+            }
         })
             .then(res => {
                 console.log(postType)

a4bb41f1f093b6689460d00a7f74615493464c5e
Noland Seigler <nolandseigler@gmail.com>
2020-03-31 22:20:55 -0500

    delete working. made some quick js for ratings so page doesnt completely refresh.

--- src/main/resources/static/js/delete.js
+++ src/main/resources/static/js/delete.js
@@ -1,25 +1,20 @@
-
-// const token = $("meta[name='_csrf']").attr("content");
-// const header = $("meta[name='_csrf_header']").attr("content");
-const token = document.querySelector('meta[name="_csrf"]').content;
-// const header = document.querySelector('meta[name="_csrf_header"]').content;
-let deleteButtonClass = document.querySelectorAll(".delete-button");
-deleteButtonClass.forEach(button => {
-    button.addEventListener("click", function() {
-        console.log("clicked")
-        let idSplit = this.getAttribute("id").split("-");
-        let postType = idSplit[0] + "-" + idSplit[1];
-        let postId = idSplit[2];
-        fetch(`http://localhost:8080/${postType}/delete?id=${postId}`, {
-            method: 'DELETE',
-            headers: {
-                'X-CSRF-TOKEN': token
-            }
-        })
-            .then(res => {
-                console.log(postType)
-                console.log(postId)
-                // location.reload(true);
+(function () {
+    const token = document.querySelector('meta[name="_csrf"]').content;
+    let deleteButtonClass = document.querySelectorAll(".delete-button");
+    deleteButtonClass.forEach(button => {
+        button.addEventListener("click", function() {
+            let idSplit = this.getAttribute("id").split("-");
+            let postType = idSplit[0] + "-" + idSplit[1];
+            let postId = idSplit[2];
+            fetch(`http://localhost:8080/${postType}/delete?id=${postId}`, {
+                method: 'DELETE',
+                headers: {
+                    'X-CSRF-TOKEN': token
+                }
             })
+                .then(res => {
+                    location.reload(true);
+                })
+        })
     })
-})
+})();

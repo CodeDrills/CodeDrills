50bcb63c228739666dfa68f2251d16ad2d727955
William Stephens <william.a.stephens17@gmail.com>
2020-04-02 13:59:10 -0500

    JS Ratings work for comments.

--- src/main/java/com/codeon/repositories/PostCommentRepo.java
+++ src/main/java/com/codeon/repositories/PostCommentRepo.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostCommentRepo extends JpaRepository<PostComment, Long> {
+    public PostComment findPostCommentById(long id);
     public PostComment getPostCommentById(Long id);
     public List<PostComment> getAllByUser_Id(Long id);
 }

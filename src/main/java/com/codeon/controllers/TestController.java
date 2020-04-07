e446329acbe93c2c3b824510952f167a003b7269
Noland Seigler <nolandseigler@gmail.com>
2020-04-07 16:13:20 -0500

    cleaned

--- src/main/java/com/codeon/controllers/TestController.java
+++ src/main/java/com/codeon/controllers/TestController.java
@@ -8,27 +8,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.security.Principal;
 
 @Controller
 public class TestController {
 
-    @Value("${firebase.api.config}")
-    private String firebaseKey;
-
     @Value("${talkjs.app.id}")
     private String talkJSAppId;
 
     private UserRepo userDao;
-    private PostRepo postDao;
 
-    public TestController(UserRepo userDao, PostRepo postDao) {
+
+    public TestController(UserRepo userDao) {
         this.userDao = userDao;
-        this.postDao = postDao;
     }
-    //comment to make it where i can commit
+
     @GetMapping("/test/{otherUserId}")
     public String getTestHtml(Model model, Principal principal, @PathVariable Long otherUserId) {
         model.addAttribute("user", userDao.findUserByUsername(principal.getName()));

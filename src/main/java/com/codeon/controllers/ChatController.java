b0ef5cb2fb9b3729a6892f526df26f41fd69ca63
Noland Seigler <nolandseigler@gmail.com>
2020-04-08 18:13:03 -0500

    inbox working. search user feature working next is chat with a user from search.

--- src/main/java/com/codeon/controllers/ChatController.java
+++ src/main/java/com/codeon/controllers/ChatController.java
@@ -8,9 +8,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
+import java.lang.reflect.Array;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ChatController {
@@ -32,19 +36,30 @@ public class ChatController {
         model.addAttribute("talkJSAppId", talkJSAppId);
         return "chat/test-modal";
     }
-    @GetMapping("/chat/inbox")
-    public String getInbox(Model model, Principal principal, @RequestParam(required = false) Long with) {
-        model.addAttribute("user", userDao.findUserByUsername(principal.getName()));
-        User otherUser = with != null ? userDao.findUserById(with) : null;
-        model.addAttribute("otherUser", otherUser);
-        model.addAttribute("talkJSAppId", talkJSAppId);
-        return "chat/inbox";
-    }
     @GetMapping("/chat/user-select")
     public String showAllUsers(Model model, Principal principal) {
         model.addAttribute("user", userDao.findUserByUsername(principal.getName()));
         model.addAttribute("userList", userDao.findAll());
         return "chat/user-select";
+    }
+
+    @GetMapping("/chat/inbox")
+    public String getInbox(Model model, Principal principal) {
+        model.addAttribute("user", userDao.findUserByUsername(principal.getName()));
+        model.addAttribute("talkJSAppId", talkJSAppId);
+        return "chat/inbox";
+    }
+    @GetMapping("/chat")
+    @ResponseBody
+    public List<User> getBothUsers(Principal principal, @RequestParam(required = false) String username, @RequestParam(required = false) Long id) {
+        if (username != null && id == null) {
+            List<User> outputList = new ArrayList<>(List.of(userDao.findUserByUsername(principal.getName())));
+            outputList.addAll(userDao.findByUsernameContaining(username));
+            return outputList;
+        } else if (username == null && id != null) {
+            return new ArrayList<>(List.of(userDao.findUserByUsername(principal.getName()), userDao.findUserById(id)));
+        }
+        return new ArrayList<>();
     }
 
 }
